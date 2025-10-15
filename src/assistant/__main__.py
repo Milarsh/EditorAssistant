a55bf8ece53156b892399229c496e71790eea3e4
Milarsh <milarshx@gmail.com>
2025-10-15 12:47:31 +0300

    schema init error catching

--- src/assistant/__main__.py
+++ src/assistant/__main__.py
@@ -1,6 +1,19 @@
+from sqlalchemy.exc import OperationalError, ProgrammingError
 from .server import main
-from .db import create_schema
+from .db import create_schema, schema_exists
 
 if __name__ == "__main__":
-    create_schema()
+    try:
+        if schema_exists():
+            print("DB schema already exists")
+        else:
+            schema = create_schema()
+            print("DB schema created")
+    except OperationalError as error:
+        print(f"DB schema is not ready: {error}")
+    except ProgrammingError as error:
+        print(f"DB schema init programming error: {error}")
+    except Exception as exception:
+        print(f"DB schema init unexpected error: {exception}")
+
     main()
